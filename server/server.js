dd7f37196c68ab02165d66e28412d352f5604153
jenn-renken <88343948+jenn-renken@users.noreply.github.com>
2022-01-13 14:32:00 -0500

    added routing, thoughts and friends

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
+const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
 
-const { typeDefs, resolvers } = require('./schemas');
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -12,8 +12,8 @@ const app = express();
 const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
-    resolvers, 
-    context: authMiddleware
+    resolvers,
+    context: authMiddleware,
   });
   await server.start();
   server.applyMiddleware({ app });
@@ -25,7 +25,7 @@ startServer()
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// serve up static assets
+// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
